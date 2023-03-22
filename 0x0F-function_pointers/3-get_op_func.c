add668be632bc812180d1f55a8c9b5b33d213e19
Leal <tshepho365@gmail.com>
2023-03-22 11:17:24 -0700

    All tasks

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,61 +1,75 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * op_add - funtion to add
- * @a: input 1
- * @b: input 2
- * Return: sum a and b
+ * op_add - ...
+ * @a: ...
+ * @b: ...
+ *
+ * Return: ...
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - funtion to subtract
- * @a : input 1
- * @b: input 2
- * Return: difference of a and b
+ * op_sub - ...
+ * @a: ...
+ * @b: ...
+ *
+ * Return: ...
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - funtion to multiplies
- * @a: input 1
- * @b: input 2
- * Return: product of a and b
+ * op_mul - ...
+ * @a: ...
+ * @b: ...
+ *
+ *  Return: ...
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - function to div
- * @a: input 1
- * @b: input 2
- * Return: division of a by b
+ * op_div - ...
+ * @a: ...
+ * @b: ...
+ *
+ *  Return: ...
  */
-
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a / b);
 }
 
 /**
- * op_mod - function to mod
- * @a: input 1
- * @b: input 2
- * Return: remainder of the division of a
+ * op_mod - ...
+ * @a: ...
+ * @b: ...
+ *
+ * Return: ...
  */
-
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a % b);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,60 +1,50 @@
 #include "3-calc.h"
-
 /**
- * op_add - funtion to add
- * @a: input 1
- * @b: input 2
- * Return: sum a and b
+ * op_add - returns the sum of a and b
+ * @a: first operand
+ * @b: the second operand
+ * Return: the sum
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - funtion to subtract
- * @a : input 1
- * @b: input 2
- * Return: difference of a and b
+ * op_sub - returns the difference of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: the difference
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - funtion to multiplies
- * @a: input 1
- * @b: input 2
- * Return: product of a and b
+ * op_mul - returns the product of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: the product
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - function to div
- * @a: input 1
- * @b: input 2
- * Return: division of a by b
+ * op_div - returns the result of the division of a by b
+ * @a: first operand
+ * @b: second operand
+ * Return: the quotient
  */
-
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
 /**
- * op_mod - function to mod
- * @a: input 1
- * @b: input 2
- * Return: remainder of the division of a
+ * op_mod - returns the remainder of the division of a by b
+ * @a: first operand
+ * @b: second operand
+ * Return: the remainder as integer
  */
-
 int op_mod(int a, int b)
 {
 	return (a % b);
