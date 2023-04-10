1ada3db1a372e8f5ca01ac9834b7787094e1d7e4
Lealsek <tshepho365@gmail.com>
2023-04-10 10:40:09 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 /**
  * read_textfile - reads a text file and prints it to the standard output
  * @filename: name of the file to be read
